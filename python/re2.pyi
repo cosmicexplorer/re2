6167611592679b9255ed0af700ce91445533199d
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2024-06-08 06:46:40 -0400

    add initial attempt at mypy harness (it fails)

---
+++ python/re2.pyi
@@ -0,0 +1,8 @@
+# Copyright 2024 The RE2 Authors.  All Rights Reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from __future__ import annotations
+
+def compile(pattern: str, options: Options | None) -> _Regexp:
+    ...
